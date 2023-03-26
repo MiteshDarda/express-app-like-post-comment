ec336550d64228f94e9d0d8a2326f566fe350784
mitesh darda <mitesh.darda.0@gmail.com>
2023-03-26 10:23:51 +0530

    fixing gitignore

--- dist/express/controllers/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const addUsers = async (req, res) => {
-    res.send("ADDING");
-};
-const deleteUsers = async (req, res) => {
-    res.send("Deliting");
-};
-//# sourceMappingURL=users.js.map
